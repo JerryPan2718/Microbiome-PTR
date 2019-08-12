57d4d7509b08a00fbed3c84cfa32f8150ebafe55
Jerry Pan <37657480+JerryPan2718@users.noreply.github.com>
2019-08-12 15:52:49 -0700

    Delete File_Name_Extraction.R

--- File_Name_Extraction.R
+++
@@ -1,6 +0,0 @@
-# Extract the file names under one directory
-File_Name_List <- list.files(path = "/Users/jerrypan/Desktop/GRIPS/Data/Citrobacter_Rodentium",
-           full.names = TRUE, include.dirs = TRUE)
-for (i in 1:length(File_Name_List)){
-  write(File_Name_List[i], file = paste("/Users/jerrypan/Desktop/GRIPS/Data/Test_Index/", basename(File_Name_List[i]),"_basename"))
-}
